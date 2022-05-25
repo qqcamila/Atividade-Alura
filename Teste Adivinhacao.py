fef45eecab2e9d62279cb4d7ed6e1626f9031faf
Camila <86685570+qqcamila@users.noreply.github.com>
2022-05-25 20:34:52 -0300

    Add files via upload
    Atividade Alura, teste de adivinhação.

---
+++ Teste Adivinhacao.py
@@ -0,0 +1,12 @@
+print("Quantos anos tem o sky?\n A) 1\n B) 2\n C) 3")
+
+idade_real = 1
+
+idade = input("Resposta: ")
+
+idade_chute = int(idade)
+
+if(idade_real == idade_chute):
+    print("Correto!")
+else:
+    print("Você errou!")
